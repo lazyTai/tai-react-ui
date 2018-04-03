9a3642b87ef045b31b70e1d2ae04c0ecd54ecf2a
lazyTai <1352983234@qq.com>
2018-04-03 08:55:01 +0800

    项目build

--- build/webpack.base.js
+++ build/webpack.base.js
@@ -2,11 +2,13 @@ var Webpack = require('webpack')
 var path = require('path')
 module.exports = {
     entry: {
-        demo: path.resolve("./src/demo/index.js")
+        demo: path.resolve("./src/demo/index.js"),
     },
+    resolve: { alias: {} },
     output: {
         path: path.resolve('./dist/'),
         filename: '[name]/[name].js',
         publicPath: '/',
-    }
+    },
+    plugins: []
 }
--- src/demo/index.js
+++ src/demo/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-import Routes from './Routes'
+import Routes from './Routes.jsx'
 
 import { HashRouter } from 'react-router-dom';
 
